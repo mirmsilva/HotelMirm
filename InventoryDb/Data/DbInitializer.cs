671afa61fcafde0712507c6d24e131aebddc4c68
miriam silva <miriam-silva@live.com>
2021-10-26 14:19:12 -0700

    Hotel Inventory MVC

--- InventoryDb/Data/DbInitializer.cs
+++ InventoryDb/Data/DbInitializer.cs
@@ -8,6 +8,7 @@ namespace InventoryDb.Data
 {
     public static class DbInitializer
     {
+        //This will check if a db exists if it does it will do nothing. If it doesn't it will load it with the test data below.
         public static void Initialize( InventoryContext context)
         {
             context.Database.EnsureCreated();
@@ -25,6 +26,31 @@ namespace InventoryDb.Data
             foreach (Hotel h in hotels)
             {
                 context.Hotels.Add(h);
+            }
+            context.SaveChanges();
+
+            var amenities = new Amenity[]
+            {
+                new Amenity{ID=1, Type = "Microwave" },
+                new Amenity{ID=2, Type = "Coffee Maker" },
+                new Amenity{ID=3, Type = "Fridge" },
+            };
+            foreach(Amenity a in amenities)
+            {
+                context.Amenities.Add(a);
+            }
+            context.SaveChanges();
+
+            var rooms = new Room[]
+            {
+                new Room{HotelID=1, AmenityID=1},
+                new Room{HotelID=1, AmenityID=2},
+                new Room{HotelID=2, AmenityID=1},
+                new Room{HotelID=2, AmenityID=3},
+            };
+            foreach(Room r in rooms)
+            {
+                context.Rooms.Add(r);
             }
             context.SaveChanges();
         }
