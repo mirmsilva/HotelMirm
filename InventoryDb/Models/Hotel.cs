dcd078d399f69b375cd4dab119016eec1fbd2dfd
miriam silva <miriam-silva@live.com>
2021-10-26 11:22:56 -0700

    Added Classes(Hotel, Room, Amenities)

---
+++ InventoryDb/Models/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryDb.Models
+{
+    public class Hotel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        //NAVIGATION
+        public ICollection<Room> Rooms { get; set; }
+    }
+   
+}
