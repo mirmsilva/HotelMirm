fc48bb8d0a275205c46e72aa2529b8973b45dc10
miriam silva <miriam-silva@live.com>
2021-10-26 11:59:47 -0700

    Added Hotel Data

---
+++ InventoryDb/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using InventoryDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryDb.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize( InventoryContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Hotels.Any())
+            {
+                return;
+            }
+
+            var hotels = new Hotel[]
+            {
+                new Hotel{ Name="City Explorer", City="Seattle", State="WA"},
+                new Hotel{ Name="Pearl Inn", City="Portland", State="OR"},
+            };
+            foreach (Hotel h in hotels)
+            {
+                context.Hotels.Add(h);
+            }
+            context.SaveChanges();
+        }
+    }
+}
